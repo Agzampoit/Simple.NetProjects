9686549c3b09e3ba9110f5f9015c431f924b2980
KostyaPolhovskiy <Kosty57115711@mail.ru>
2019-01-12 14:10:43 +0300

    Add Ready to use LogBuffer

---
+++ LogBuffer/LogBuffer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace LogBuffer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LogBuffer buf = new LogBuffer(30, 10000);
+            //buf.Add("ПРивет  1");
+            //buf.Add("ПРивет  2 ");
+            //buf.Add("Привет 3");
+
+            for (int i = 1; i <= 10000; i++)
+            {
+                buf.Add(i.ToString());
+            }
+            Console.WriteLine("Ready, Enter...");
+            Console.ReadLine();
+
+            Tester test1 = new Tester();
+            Console.WriteLine("File is correct " + test1.IsCorrect());
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ LogBuffer/LogBuffer/Tester.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace LogBuffer
+{
+    class Tester
+    {
+        private string path;
+        
+        public Tester(string path = @"C:\Users\Kostya\source\repos\LogBuffer\List.txt" )
+        {
+            this.path = path;
+        }
+
+        public Boolean IsCorrect()
+        {
+            string line;
+            int i = 0;
+
+            StreamReader file = new StreamReader(path);
+            while ((line = file.ReadLine()) != null)
+            {
+                i += 1;
+                if (i != Convert.ToInt32(line))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
